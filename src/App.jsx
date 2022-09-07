f9c43f17d7a303c235a6069d1d1bd92cabc4d592
jandersonvb <jandersonvb.dev@gmail.com>
2022-09-06 21:05:43 -0300

    feat: create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 import './global.css'
 
+import { Post } from './Post'
+
 export function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar>Sidebar</Sidebar>
+        <main>
+          <Post author="John Smith" content="John Smith is the author of the post" />
+          <Post author="John Doe" content="John Doe is the author of the post" />
+          <Post author="Gabriel Smith" content="Gabriel Smith is the author of the post" />
+        </main>
+      </div>
+    </div>
   )
 }
 
