d6fae757786a0aa1be905bce7c719b44892d4ae3
jandersonvb <jandersonvb.dev@gmail.com>
2022-09-08 13:48:39 -0300

    feat: finishing sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from './Sidebar.module.css'
 
+import { PencilLine } from 'phosphor-react'
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -8,12 +10,16 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1593720219276-0b1eacd0aef4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/jandersonvb.png" />
         <strong>Janderson Vilas Boas</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="http://">Editar seu perfil</a>
+        <a href="http://">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
       </footer>
 
     </aside>
